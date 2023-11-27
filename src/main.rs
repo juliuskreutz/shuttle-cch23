f48c7888e3f67aa6d82aed0d5a1b62ec612c4eec
Julius Kreutz <julius@kreutz.dev>
2023-11-27 18:54:30 +0100

    Added error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web::ServiceConfig, HttpResponse, Responder};
 use shuttle_actix_web::ShuttleActixWeb;
 
 #[get("/")]
@@ -6,10 +6,15 @@ async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/-1/error")]
+async fn error() -> impl Responder {
+    HttpResponse::InternalServerError()
+}
+
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.service(hello_world).service(error);
     };
 
     Ok(config.into())
