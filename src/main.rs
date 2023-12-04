e0785444f9163b4c2dcbcd36b8809a3b524efda2
Julius Kreutz <julius@kreutz.dev>
2023-12-04 13:14:52 +0100

    Day04

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod day00;
 mod day01;
+mod day04;
 
 use actix_web::web::ServiceConfig;
 use shuttle_actix_web::ShuttleActixWeb;
@@ -7,7 +8,9 @@ use shuttle_actix_web::ShuttleActixWeb;
 #[shuttle_runtime::main]
 async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.configure(day00::configure).configure(day01::configure);
+        cfg.configure(day00::configure)
+            .configure(day01::configure)
+            .configure(day04::configure);
     };
 
     Ok(config.into())
