0c912ffb00bb3be770ec20be4a7129725982f25f
Julius Kreutz <julius@kreutz.dev>
2023-12-07 00:22:14 +0100

    Clean up

--- src/day06.rs
+++ src/day06.rs
@@ -1,17 +1,11 @@
-use actix_web::{
-    post,
-    web::{self, ServiceConfig},
-    HttpResponse, Responder,
-};
+use actix_web::{post, web::ServiceConfig, HttpResponse, Responder};
 
 pub fn configure(cfg: &mut ServiceConfig) {
     cfg.service(index);
 }
 
 #[post("/6")]
-async fn index(request: web::Bytes) -> impl Responder {
-    let s = String::from_utf8(request.to_vec()).unwrap();
-
+async fn index(s: String) -> impl Responder {
     let elf = s.matches("elf").count();
     let elf_on_shelf = s.matches("elf on a shelf").count();
     let elf_no_shelf = s.matches("shelf").count() - elf_on_shelf;
