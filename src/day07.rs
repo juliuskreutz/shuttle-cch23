227c5a623efdbf614f24052f65252fa288d5de4e
Julius Kreutz <julius@kreutz.dev>
2023-12-07 15:26:48 +0100

    Day07 3

--- src/day07.rs
+++ src/day07.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use actix_web::{get, web::ServiceConfig, HttpRequest, HttpResponse, Responder};
 use anyhow::Context;
 use base64::{engine::general_purpose, Engine};
@@ -21,19 +23,8 @@ async fn decode(request: HttpRequest) -> ShuttleResult<impl Responder> {
 
 #[derive(serde::Deserialize)]
 struct Recipe {
-    recipe: Ingredients,
-    pantry: Ingredients,
-}
-
-#[derive(serde::Serialize, serde::Deserialize)]
-struct Ingredients {
-    flour: usize,
-    sugar: usize,
-    butter: usize,
-    #[serde(rename = "baking powder")]
-    baking_powder: usize,
-    #[serde(rename = "chocolate chips")]
-    chocolate_chips: usize,
+    recipe: HashMap<String, usize>,
+    pantry: HashMap<String, usize>,
 }
 
 #[get("/7/bake")]
@@ -45,21 +36,16 @@ async fn bake(request: HttpRequest) -> ShuttleResult<impl Responder> {
     let mut recipe: Recipe = serde_json::from_str(&decoded)?;
 
     let mut count = 0;
-    loop {
-        if recipe.recipe.flour > recipe.pantry.flour
-            || recipe.recipe.sugar > recipe.pantry.sugar
-            || recipe.recipe.butter > recipe.pantry.butter
-            || recipe.recipe.baking_powder > recipe.pantry.baking_powder
-            || recipe.recipe.chocolate_chips > recipe.pantry.chocolate_chips
-        {
-            break;
+    'outer: loop {
+        for (key, &value) in &recipe.recipe {
+            if recipe.pantry.get(key).copied().unwrap_or_default() < value {
+                break 'outer;
+            }
         }
 
-        recipe.pantry.flour -= recipe.recipe.flour;
-        recipe.pantry.sugar -= recipe.recipe.sugar;
-        recipe.pantry.butter -= recipe.recipe.butter;
-        recipe.pantry.baking_powder -= recipe.recipe.baking_powder;
-        recipe.pantry.chocolate_chips -= recipe.recipe.chocolate_chips;
+        for (key, &value) in &recipe.recipe {
+            *recipe.pantry.get_mut(key).unwrap() -= value;
+        }
 
         count += 1;
     }
