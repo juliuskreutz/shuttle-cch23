4851da6ac01b6413ee3f8058153bc4438df482d8
Julius Kreutz <julius@kreutz.dev>
2023-12-13 14:22:08 +0100

    Day13

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,19 @@ mod day07;
 mod day08;
 mod day11;
 mod day12;
+mod day13;
 
-use actix_web::web::ServiceConfig;
+use actix_web::web::{Data, ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
+use sqlx::PgPool;
 
 type ShuttleResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 #[shuttle_runtime::main]
-async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
+async fn main(
+    #[shuttle_shared_db::Postgres(local_uri = "postgres://postgres:@localhost/shuttle")]
+    pool: PgPool,
+) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     let config = move |cfg: &mut ServiceConfig| {
         cfg.configure(day00::configure)
             .configure(day01::configure)
@@ -24,7 +29,9 @@ async fn main() -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clon
             .configure(day07::configure)
             .configure(day08::configure)
             .configure(day11::configure)
-            .configure(day12::configure);
+            .configure(day12::configure)
+            .configure(day13::configure)
+            .app_data(Data::new(pool.clone()));
     };
 
     Ok(config.into())
